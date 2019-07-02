3ecefaa0a942d75cf7c27e07479f073572693fff
Oskar Sharipov <oskar.sharipov@tuta.io>
2019-07-02 14:04:45 +0300

    Move wiki mirror host variable to django settings
    To get it use django.conf.settings.WIKI_MIRROR_HOST

--- wiki/get_wiki_page.py
+++ wiki/get_wiki_page.py
@@ -1,11 +1,12 @@
 # from django.
 from django.http import HttpResponse
+from django.conf import settings
 
 import requests
 
 
 def get(request, title_name):
     return HttpResponse(
-        requests.get('http://10.5.200.206:9454/' + title_name).text
+        requests.get(settings.WIKI_MIRROR_HOST + title_name).text
     )
     # return HttpResponse(title_name)
