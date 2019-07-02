6aabce64881d1c509b53110cbbcd01779ee4d362
Oskar Sharipov <oskar.sharipov@tuta.io>
2019-07-02 17:26:09 +0300

    Add counter of steps
    Counter uses sessions

--- wiki/get_wiki_page.py
+++ wiki/get_wiki_page.py
@@ -5,7 +5,16 @@ import requests
 
 
 def get(request, title_name):
+    if request.session.get('steps', None) is None:
+        request.session['steps'] = 0
+        return HttpResponse('New game!')
+
     wiki_page_request = requests.get(settings.WIKI_MIRROR_HOST + title_name)
+
+    if wiki_page_request.ok and\
+            wiki_page_request.headers['content-type'] == 'text/html':
+        request.session['steps'] += 1
+
     return HttpResponse(
         wiki_page_request.content,
         content_type=wiki_page_request.headers['content-type']
