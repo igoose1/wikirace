f78f73971d96434a413b345b7e0ffee578140d4d
NikPeg <peganov.nik@gmail.com>
2019-07-03 14:02:07 +0300

    Load web-pages from graph
    User can go on links. Program prints start and finish pages, count of
    steps. Find one bug: page doesn't view some links.

--- wiki/get_wiki_page.py
+++ wiki/get_wiki_page.py
@@ -1,38 +1,44 @@
 from django.http import HttpResponse
 from django.conf import settings
-
-from django.template import loader
-
+from wiki.GameOperator import GameOperator
 import requests
+from wiki.ZIMFile import MyZIMFile
+from wiki.GraphReader import GraphReader
 
 
 def get(request, title_name):
-    if request.session.get('steps', None) is None:
-        request.session['steps'] = 0
-        return HttpResponse('New game!')
-
-    wiki_page_request = requests.get(settings.WIKI_MIRROR_HOST + title_name)
-
-    if wiki_page_request.ok and\
-            wiki_page_request.headers['content-type'] == 'text/html':
-        request.session['steps'] += 1
-        template = loader.get_template('wiki/frame.html')
-        _from = 'start_page'
-        to = 'finish_page'
-        counter = request.session['steps']
-        context = {
-            'from': _from,
-            'to': to,
-            'counter': counter,
-        }
-        return HttpResponse(
-            template.render(context, request),
-            content_type=wiki_page_request.headers['content-type'],
-            status=wiki_page_request.status_code
-        )
-
-    return HttpResponse(
-        wiki_page_request.content,
-        content_type=wiki_page_request.headers['content-type'],
-        status=wiki_page_request.status_code
-    )
+	print("req") #delete
+	zim = MyZIMFile('F:\\LKSH\\New\\by_category\\wikipedia_ru_geography_nopic_2019-05.zim')
+	graph = GraphReader('F:\\LKSH\\offset0', 'F:\\LKSH\\edges0')  # later
+	currOperator = GameOperator(zim, graph)
+	if request.session.get('operator', None) is None:
+		# начало игры
+		currOperator.initialize_game()
+		request.session['steps'] = 0
+		request.session['operator'] = currOperator.save()
+		print('Start')
+	else:
+		currOperator.load(request.session['operator'])
+	print('INFO', currOperator.current_page_id, currOperator.end_page_id, request.session['steps'])
+	# nextPage(title_name) - True - конец игры False - нет None - не статья
+	requested_page = zim.get_by_url('/' + title_name)
+	
+	if not requested_page:
+		wiki_page = zim._get_article_by_index(currOperator.current_page_id)
+		return HttpResponse(wiki_page.data, wiki_page.mimetype)
+	if (requested_page and requested_page.namespace == "A"):
+		request.session['steps'] += 1
+		result = currOperator.next_page('/' + title_name)
+		request.session['operator'] = currOperator.save()
+		if result == True:
+			return HttpResponse('You win!')
+		elif result is None:
+			return HttpResponse('Error! Not article!')
+		# wiki_page_request = requests.get(settings.WIKI_MIRROR_HOST + title_name)  # здесь вместо title_name current_page_id
+		wiki_page = zim._get_article_by_index(currOperator.current_page_id)
+		# mimetype - тип. У статьи - text/html
+		print("shvid")
+		return HttpResponse(wiki_page.data, wiki_page.mimetype)
+	
+	return HttpResponse(requested_page.data, requested_page.mimetype)
+	
