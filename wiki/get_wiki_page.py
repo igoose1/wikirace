a68917fcca9f3b752dea7d61f6ebf72079bcd20d
ArtoLord <artgoi111@gmail.com>
2019-07-04 10:55:41 +0300

    refactoring get_wiki_page and adding statistics

--- wiki/get_wiki_page.py
+++ wiki/get_wiki_page.py
@@ -1,44 +1,49 @@
 from django.http import HttpResponse
 from django.conf import settings
 from wiki.GameOperator import GameOperator
-import requests
+from wiki.models import Game
 from wiki.ZIMFile import MyZIMFile
 from wiki.GraphReader import GraphReader
 
 
 def get(request, title_name):
-	print("req") #delete
-	zim = MyZIMFile('F:\\LKSH\\New\\by_category\\wikipedia_ru_geography_nopic_2019-05.zim')
-	graph = GraphReader('F:\\LKSH\\offset0', 'F:\\LKSH\\edges0')  # later
-	currOperator = GameOperator(zim, graph)
-	if request.session.get('operator', None) is None:
-		# начало игры
-		currOperator.initialize_game()
-		request.session['steps'] = 0
-		request.session['operator'] = currOperator.save()
-		print('Start')
-	else:
-		currOperator.load(request.session['operator'])
-	print('INFO', currOperator.current_page_id, currOperator.end_page_id, request.session['steps'])
-	# nextPage(title_name) - True - конец игры False - нет None - не статья
-	requested_page = zim.get_by_url('/' + title_name)
-	
-	if not requested_page:
-		wiki_page = zim._get_article_by_index(currOperator.current_page_id)
-		return HttpResponse(wiki_page.data, wiki_page.mimetype)
-	if (requested_page and requested_page.namespace == "A"):
-		request.session['steps'] += 1
-		result = currOperator.next_page('/' + title_name)
-		request.session['operator'] = currOperator.save()
-		if result == True:
-			return HttpResponse('You win!')
-		elif result is None:
-			return HttpResponse('Error! Not article!')
-		# wiki_page_request = requests.get(settings.WIKI_MIRROR_HOST + title_name)  # здесь вместо title_name current_page_id
-		wiki_page = zim._get_article_by_index(currOperator.current_page_id)
-		# mimetype - тип. У статьи - text/html
-		print("shvid")
-		return HttpResponse(wiki_page.data, wiki_page.mimetype)
-	
-	return HttpResponse(requested_page.data, requested_page.mimetype)
-	
+    print("req")  # delete
+    zim = MyZIMFile(settings.WIKI_DATA)
+    graph = GraphReader(settings.GRAPH_DIR + '/offset0', settings.GRAPH_DIR + '/edges0')  # later
+    currOperator = GameOperator(zim, graph)
+    if request.session.get('operator', None) is None:
+        # начало игры
+        currOperator.initialize_game()
+        Game.objects.create(session_id=request.session.session_key,
+                            first=currOperator.current_page_id,
+                            last=currOperator.end_page_id,
+                            steps=0)
+        request.session['steps'] = 0
+        request.session['operator'] = currOperator.save()
+        print('Start')
+    else:
+        currOperator.load(request.session['operator'])
+    print('INFO', currOperator.current_page_id, currOperator.end_page_id, request.session['steps'])
+    # nextPage(title_name) - True - конец игры False - нет None - не статья
+    requested_page = zim.get_by_url('/' + title_name)
+
+    if not requested_page:
+        wiki_page = zim._get_article_by_index(currOperator.current_page_id)
+        return HttpResponse(wiki_page.data, wiki_page.mimetype)
+    if (requested_page and requested_page.namespace == "A"):
+        request.session['steps'] += 1
+        result = currOperator.next_page('/' + title_name)
+        request.session['operator'] = currOperator.save()
+        curr_game = Game.objects.filter(session_id=request.session.session_key)[0]
+        curr_game.steps = request.session['steps']
+        curr_game.ended = result
+        curr_game.update()
+        if result == True:
+            return HttpResponse('You win!')
+        elif result is None:
+            return HttpResponse('Error! Not article!')
+        wiki_page = zim._get_article_by_index(currOperator.current_page_id)
+        # mimetype - тип. У статьи - text/html
+        return HttpResponse(wiki_page.data, wiki_page.mimetype)
+
+    return HttpResponse(requested_page.data, requested_page.mimetype)
--- wiki/models.py
+++ wiki/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Game(models.Model):
-    first = models.CharField(max_length = 100)
-    last = models.CharField(max_length = 100)
+    first = models.IntegerField()
+    last = models.IntegerField()
+    session_id = models.CharField(unique=True)
     steps = models.IntegerField()
     ended = models.BooleanField()
-
