a980456399d0e2752377233b0d70b20ef8f0b975
Oskar Sharipov <oskar.sharipov@tuta.io>
2019-07-05 17:10:56 +0300

    Remove strange lines of text

--- wiki/urls.py
+++ wiki/urls.py
@@ -2,9 +2,7 @@ from django.conf.urls import url
 
 from . import get_wiki_page
 
-"""
 
-    """
 urlpatterns = [
     url('main', get_wiki_page.get_main_page),
     url('game_start', get_wiki_page.get_start),
