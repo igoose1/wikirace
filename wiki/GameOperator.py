7727f749c9fe7498f8d629db14f01d7e6d4324a4
Oskar Sharipov <oskar.sharipov@tuta.io>
2019-07-05 17:21:25 +0300

    Remove space which breaks program

--- wiki/GameOperator.py
+++ wiki/GameOperator.py
@@ -107,7 +107,7 @@ class GameOperator:
                     self.history = self.history[:len(self.history) - 1 - self.history[::-1].index(idx)]
                 else:
                     return False 
-             if (self.current_page_id != idx):
+            if (self.current_page_id != idx):
                 self.steps += 1
                 self.current_page_id = idx
             if not self.history or idx != self.history[-1]:
