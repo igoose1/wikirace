45c7a324b29a310eae3b96255c9134ee7b0a80e1
Kate <tochilina.2002@yandex.ru>
2019-07-05 16:50:22 +0300

    merge_precalcs ready

--- wiki/merge_precalcs.py
+++ wiki/merge_precalcs.py
@@ -1,4 +1,4 @@
-from GraphReader import _bytes_to_int
+from GraphReader import _bytes_to_int, _int_to_bytes
 import sys
 
 N = 5054753
@@ -23,13 +23,39 @@ def get_variants_for_vertex(v, input_filename, input_files_amount):
 
 
 files_amount = 1
-print(get_variants_for_vertex(int(sys.argv[1]), 'data/good_end', files_amount))
-#for v in range(N):
-#    d = get_variants_for_vertex(v, 'data/good_end', files_amount)
+#print(get_variants_for_vertex(int(sys.argv[1]), 'data/good_end', files_amount))
+easy_pairs = []
+medium_pairs = []
+hard_pairs = []
 
+for v in range(N):
+    if v % 1000 == 0:
+        print(v, 'ready')
+    d = get_variants_for_vertex(v, 'data/good_end', files_amount)
+    for u in d['easy']:
+        easy_pairs.append([v, u])
+    for u in d['medium']:
+        medium_pairs.append([v, u])
+    for u in d['hard']:
+        hard_pairs.append([v, u])
 
+hard = open('data/hard', 'wb')
+hard.write(_int_to_bytes(len(hard_pairs)))
+for p in hard_pairs:
+    hard.write(_int_to_bytes(p[0]))
+    hard.write(_int_to_bytes(p[1]))
+hard.close()
 
-        
-        
-            
-        
+medium = open('data/medium', 'wb')
+medium.write(_int_to_bytes(len(medium_pairs)))
+for p in hard_pairs:
+    medium.write(_int_to_bytes(p[0]))
+    medium.write(_int_to_bytes(p[1]))
+medium.close()
+
+easy = open('data/easy', 'wb')
+easy.write(_int_to_bytes(len(easy_pairs)))
+for p in easy_pairs:
+    easy.write(_int_to_bytes(p[0]))
+    easy.write(_int_to_bytes(p[1]))
+easy.close()
