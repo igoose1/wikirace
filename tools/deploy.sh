ad5ff0f8194bfd1680681b4afb07b07c7015f3de
ArtoLord <artgoi111@gmail.com>
2019-07-05 15:04:48 +0300

    Add migrations and change gitignore

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -13,7 +13,6 @@ git fetch -q origin $BRANCH
 git reset -q --hard FETCH_HEAD
 
 $BIN/pip -qq install --index-url http://mirror.p.lksh.ru/pypi --trusted-host mirror.p.lksh.ru -r requirements.txt
-$BIN/python manage.py makemigrations
 $BIN/python manage.py migrate
 sudo /etc/init.d/uwsgi restart
 sudo /etc/init.d/nginx restart
