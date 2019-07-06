83642924e6954a5aa56df417c32fcfd6b7c11d8f
Kate <tochilina.2002@yandex.ru>
2019-07-06 10:45:55 +0300

    minor fix"

--- wiki/precalc.py
+++ wiki/precalc.py
@@ -38,6 +38,8 @@ NONE = 2**32 - 1
 dists = [[3, 4], [5, 7], [8, 11]]
 for cur_vertex in range(N):
     v = cur_vertex
+    if v % 10000 == 0:
+        print(v, 'ready')
     visited = []
     max_steps = randrange(dists[2][0], dists[2][1])
     while len(visited) < max_steps:
@@ -49,11 +51,11 @@ for cur_vertex in range(N):
     medium_steps = min(randrange(dists[1][0], dists[1][1]), len(visited))
     hard_steps = min(randrange(dists[2][0], dists[2][1]), len(visited))
     if easy_steps >= dists[0][0]:
-        easy_steps.append([cur_vertex, visisted[easy_steps - 1]])
+        easy_steps.append([cur_vertex, visited[easy_steps - 1]])
     if medium_steps >= dists[1][0]:
-        medium_steps.append([cur_vertex, visisted[medium_steps - 1]])
+        medium_steps.append([cur_vertex, visited[medium_steps - 1]])
     if hard_steps >= dists[2][0]:
-        hard_steps.append([cur_vertex, visisted[hard_steps - 1]])
+        hard_steps.append([cur_vertex, visited[hard_steps - 1]])
 
 hard = open('data/hard', 'wb')
 hard.write(_int_to_bytes(len(hard_pairs)))
