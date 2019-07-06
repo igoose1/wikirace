30b3c7b4ab86c1e9055a9cdde697e0b46fa8920b
ArtoLord <artgoi111@gmail.com>
2019-07-06 17:46:27 +0300

    Adding FeedbackAdmin

--- wiki/admin.py
+++ wiki/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
-from wiki.models import GameStat
+from wiki.models import GameStat, Feedback
 
-    
+
 class GameStatAdmin(admin.ModelAdmin):
-    list_display = ['game_id', 'start_page_id', 'end_page_id', 
-                    'steps', 'finished', 'start_time', 'last_action_time']
-    ordering = ['game_id']
-    
+	list_display = ['game_id', 'start_page_id', 'end_page_id',
+					'steps', 'finished', 'start_time', 'last_action_time']
+	ordering = ['game_id']
+
+
+class FeedbackAdmin(admin.ModelAdmin):
+	list_display = ['text', 'time']
+	ordering = ['time']
+
 
 admin.site.register(GameStat, GameStatAdmin)
+admin.site.register(Feedback, FeedbackAdmin)
