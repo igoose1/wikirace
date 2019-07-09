104ab9d2c930a3ab7b964ad74be1fce327866538
Oskar Sharipov <oskar.sharipov@tuta.io>
2019-07-09 14:14:45 +0300

    Refactor get_wiki_page
    Move declensions to wiki/declension.py,
    Fix decorators,
    Make difficulties readable.

--- wiki/get_wiki_page.py
+++ wiki/get_wiki_page.py
@@ -1,47 +1,169 @@
-from django.http import HttpResponse, HttpResponseRedirect, Http404, HttpResponseServerError
+from django.http import HttpResponse,\
+                        HttpResponseRedirect,\
+                        HttpResponseNotFound,\
+                        HttpResponseBadRequest
 from django.conf import settings
 from django.template import loader
 from django.utils import timezone
 
+from . import declension
 from .GameOperator import GameOperator
 from .GraphReader import GraphReader
 from .ZIMFile import MyZIMFile
 from .form import FeedbackForm
 
 
-def answer(func, requires_game=False,
-          load_game_operator=False, save_game_operator=False):
-    class PreVariables:
-        def __init__(self, request):
-            self.zim_file = MyZIMFile(settings.WIKI_ZIMFILE_PATH)
-            self.graph = GraphReader(
-                settings.GRAPH_OFFSET_PATH,
-                settings.GRAPH_EDGES_PATH
-            )
-            self.game_operator = GameOperator(zim_file, graph)
-            self.request = request
+class PreVariables:
+    def __init__(self, request):
+        self.zim_file = MyZIMFile(settings.WIKI_ZIMFILE_PATH)
+        self.graph = GraphReader(
+            settings.GRAPH_OFFSET_PATH,
+            settings.GRAPH_EDGES_PATH
+        )
+        self.game_operator = GameOperator(self.zim_file, self.graph)
+        self.request = request
 
+
+def requires_graph(func):
     def wrapper(request, *args, **kwargs):
         prevars = PreVariables(request)
-        session_operator = request.session.get('operator', None)
-        if requires_game and session_operator is None:
-            return HttpResponseRedirect('/')
-        if load_game_operator:
-            prevars.game_operator.load(session_operator)
-
         res = func(prevars, *args, **kwargs)
-        if save_game_operator:
-            request.session['operator'] = prevars.game_operator.save()
+        if prevars.game_operator.game is not None:
+            prevars.request.session['operator'] = prevars.game_operator.save()
+        else:
+            prevars.request.session['operator'] = None
         return res
 
     return wrapper
 
 
-@answer(requires_game=True, load_game_operator=True, save_game_operator=True)
+def requires_game(func):
+    def wrapper(prevars, *args, **kwargs):
+        session_operator = prevars.request.session.get('operator', None)
+        if session_operator is None:
+            return HttpResponseRedirect('/')
+        prevars.game_operator.load(session_operator)
+        return func(prevars, *args, **kwargs)
+
+    return wrapper
+
+
+def get_settings(settings_user):
+    default = {'difficulty': -1, 'name': 'no name'}
+    for key in default.keys():
+        settings_user[key] = settings_user.get(key, default[key])
+    return settings_user
+
+
+@requires_graph
+def get_main_page(prevars):
+    session_operator = prevars.request.session.get('operator', None)
+
+    template = loader.get_template('wiki/start_page.html')
+    context = {
+        'is_playing': session_operator and not session_operator[2],
+        'settings': get_settings(prevars.request)
+    }
+    return HttpResponse(template.render(context, prevars.request))
+
+
+@requires_graph
+def change_settings(prevars):
+    name = prevars.request.POST.get('name')
+    difficulty_names = ('random', 'easy', 'medium', 'hard')
+    if name not in difficulty_names\
+            or (isinstance(name, str) and len(name) > 16):
+        return HttpResponseBadRequest()
+
+    prevars.request.session['settings'] = {
+        'difficulty': difficulty_names.index(name),
+        'name': name
+    }
+    return HttpResponse('Ok')
+
+
+@requires_graph
+def get_start(prevars):
+    prevars.game_operator.initialize_game(
+        get_settings(
+            prevars.request.session.get('settings', dict()  )
+        )['difficulty']
+    )
+    return HttpResponseRedirect(
+        prevars.zim_file.read_directory_entry_by_index(
+            prevars.game_operator.current_page_id
+        )['url']
+    )
+
+
+@requires_graph
+@requires_game
+def get_continue(prevars):
+    return HttpResponseRedirect(
+        prevars.zim_file.read_directory_entry_by_index(
+            prevars.game_operator.current_page_id
+        )['url']
+    )
+
+
+@requires_graph
+@requires_game
+def get_back(prevars):
+    prevars.game_operator.prev_page()
+    return HttpResponseRedirect(
+        prevars.zim_file.read_directory_entry_by_index(
+            prevars.game_operator.current_page_id
+        )['url']
+    )
+
+
+@requires_graph
+@requires_game
+def get_hint_page(prevars):
+    content = prevars.zim_file.get_by_index(
+        prevars.game_operator.end_page_id
+    )
+    data, namespace, mime_type = content
+
+    template = loader.get_template('wiki/hint_page.html')
+    context = {
+        'content': data.decode('utf-8'),
+    }
+    return HttpResponse(template.render(context, prevars.request))
+
+
+@requires_graph
+@requires_game
+def winpage(prevars):
+    settings_user = get_settings(
+        prevars.request.session.get('settings', dict())
+    )
+    context = {
+        'from': prevars.zim_file.read_directory_entry_by_index(
+                prevars.game_operator.start_page_id
+            )['title'],
+        'to': prevars.zim_file.read_directory_entry_by_index(
+                prevars.game_operator.end_page_id
+            )['title'],
+        'counter': prevars.game_operator.steps,
+        'move_end': declension.mupltiple_suffix(
+                prevars.game_operator.steps
+            ),
+        'name': settings_user['name']
+    }
+    template = loader.get_template('wiki/win_page.html')
+    prevars.game_operator.game = None
+    return HttpResponse(
+        template.render(context, prevars.request),
+    )
+
+
+@requires_graph
+@requires_game
 def get(prevars, title_name):
     content = prevars.zim_file.get_by_url('/' + title_name)
     if content is None:
-        raise Http404()
+        return HttpResponseNotFound()
 
     data, namespace, mime_type = content
     if namespace != 'A':
@@ -54,108 +176,44 @@ def get(prevars, title_name):
 
     next_page_result = prevars.game_operator.next_page('/' + title_name)
 
-    if next_page_result: # TODO
-        return winpage(prevars.request) # TODO
-    elif next_page_result is None: # TODO
+    if next_page_result:
+        return winpage(prevars.request)
+    elif next_page_result is None:
         return HttpResponseRedirect(
-            zim_file.read_directory_entry_by_index(prevars.game_operator.current_page_id)['url']
-        ) # TODO
+            prevars.zim_file.read_directory_entry_by_index(
+                prevars.game_operator.current_page_id
+            )['url']
+        )
 
     template = loader.get_template('wiki/page.html')
     context = {
-        'title': zim_file.read_directory_entry_by_index(prevars.game_operator.current_page_id)['title'], # TODO
-        'from': zim_file.read_directory_entry_by_index(prevars.game_operator.start_page_id)['title'], # TODO
-        'to': zim_file.read_directory_entry_by_index(prevars.game_operator.end_page_id)['title'], # TODO
+        'title': prevars.zim_file.read_directory_entry_by_index(
+                prevars.game_operator.current_page_id
+            )['title'],
+        'from': prevars.zim_file.read_directory_entry_by_index(
+                prevars.game_operator.start_page_id
+            )['title'],
+        'to': prevars.zim_file.read_directory_entry_by_index(
+                prevars.game_operator.end_page_id
+            )['title'],
         'counter': prevars.game_operator.steps,
-        'wiki_content': zim_file.get_by_index(prevars.game_operator.current_page_id).data.decode('utf-8'), # TODO
+        'wiki_content': prevars.zim_file.get_by_index(
+                prevars.game_operator.current_page_id
+            ).data.decode('utf-8'),
         'history_empty': prevars.game_operator.is_history_empty()
     }
-    return HttpResponse(template.render(context, prevars.request), content_type=mime_type)
-
-
-@answer(save_game_operator=True)
-def get_start(prevars):
-    difficulty = get_settings(prevars.request)['difficulty']
-    prevars.game_operator.initialize_game(difficulty)
-    return HttpResponseRedirect(
-        prevars.zim_file.read_directory_entry_by_index(prevars.game_operator.current_page_id)['url']
-    )
-
-
-@answer(requires_game=True, load_game_operator=True, save_game_operator=True)
-def get_back(prevars):
-    prevars.game_operator.prev_page()
-    return HttpResponseRedirect(
-        prevars.zim_file.read_directory_entry_by_index(prevars.game_operator.current_page_id)['url']
-    )
-
-
-@answer(requires_game=True, load_game_operator=True)
-def get_continue(prevars):
-    return HttpResponseRedirect(
-        self.zim_file.read_directory_entry_by_index(self.game_operator.current_page_id)['url']
-    )
-
-
-@answer(requires_game=True, load_game_operator=True)
-def winpage(prevars):
-    ending = ''
-    if prevars.game_operator.steps % 10 == 1 and prevars.game_operator.steps % 100 != 11:
-        pass
-    elif prevars.game_operator.steps % 10 in [2, 3, 4] and prevars.game_operator.steps % 100 not in [12, 13, 14]:
-        ending = 'а'
-    else:
-        ending = 'ов'
-    settings_user = get_settings(request)
-    context = {
-        'from': prevars.zim_file.read_directory_entry_by_index(prevars.game_operator.start_page_id)['title'], # TODO
-        'to': prevars.zim_file.read_directory_entry_by_index(prevars.game_operator.end_page_id)['title'], # TODO
-        'counter': prevars.game_operator.steps,
-        'move_end': ending,
-        'name': settings_user['name']
-    }
-    template = loader.get_template('wiki/win_page.html')
-    prevars.request.session['operator'] = None
     return HttpResponse(
         template.render(context, prevars.request),
+        content_type=mime_type
     )
 
 
-@answer
-def get_main_page(prevars):
-    template = loader.get_template('wiki/start_page.html')
-    session_operator = prevars.request.session.get('operator', None)
-    is_playing = False
-    if session_operator and not session_operator[2]:
-        is_playing = True
-
-    context = {
-        'is_playing': is_playing,
-        'settings': get_settings(prevars.request)
-    }
-    return HttpResponse(template.render(context, prevars.request))
-
-
-@answer(requires_game=True, load_game_operator=True)
-def get_hint_page(request):
-    content = prevars.zim_file.get_by_index(game_operator.end_page_id)
-    data, namespace, mime_type = content
-
-    context = {
-        'content': data.decode('utf-8'),
-    }
-
-    template = loader.get_template('wiki/hint_page.html')
-    return HttpResponse(template.render(context, prevars.request))
-
-
-@answer
+@requires_graph
 def get_feedback_page(prevars):
     if prevars.request.method == "POST":
-        form = FeedbackForm(prevars.request.POST)
-        post = form.save()
-        post.time = timezone.now()
-        post.save()
+        form = FeedbackForm(prevars.request.POST).save()
+        form.time = timezone.now()
+        form.save()
         return HttpResponseRedirect('/')
     else:
         form = FeedbackForm()
@@ -164,41 +222,4 @@ def get_feedback_page(prevars):
         'form': form,
     }
     template = loader.get_template('wiki/feedback_page.html')
-    return HttpResponse(template.render(context=context, request=prevars.request))
-
-
-@answer
-def change_settings(prevars):
-    val = prevars.request.POST.get('difficulty', None)
-    name = prevars.request.POST.get('name', 'no name')
-    diff_id = get_difficulty_level_by_name(val)
-    if diff_id is None or len(name) > 16:
-        return HttpResponseServerError()
-    prevars.request.session['settings'] = {'difficulty': diff_id, 'name': name}
-    return HttpResponse('Ok')
-
-
- # TODO # TODO # TODO # TODO # TODO # TODO # TODO # TODO # TODO
- # TODO # TODO # TODO # TODO # TODO # TODO # TODO # TODO # TODO
- # TODO # TODO # TODO # TODO # TODO # TODO # TODO # TODO # TODO
-
-
-def get_difficulty_level_by_name(name):
-    if (name == 'random'):
-        return -1
-    if (name == 'easy'):
-        return 0
-    if (name == 'medium'):
-        return 1
-    if (name == 'hard'):
-        return 2
-    return None
-
-
-def get_settings(request):
-    default = {'difficulty': -1, 'name': 'no name'}
-    settings_user = request.session.get('settings', default)
-    for key in default.keys():
-        if key not in settings_user.keys():
-            settings_user[key] = default[key]
-    return settings_user
+    return HttpResponse(template.render(context, prevars.request))
