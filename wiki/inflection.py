cdfe0e0a39f63d3fb85e0702004650b39cca06d4
Oskar Sharipov <mail@mai.l>
2019-07-10 16:19:29 +0000

    Revert "Merge branch 'refactor-get-wiki-page-review' into 'master'"
    This reverts merge request !9

--- wiki/inflection.py
+++
@@ -1,7 +0,0 @@
-def mupltiple_suffix(num):
-    if num % 10 == 1 and num % 100 != 11:
-        return ''
-    elif num % 10 in (2, 3, 4) and num % 100 not in (12, 13, 14):
-        return 'а'
-    else:
-        return 'ов'
