11ffcf58cdbafa0aba0c24b440449b5287d94966
ArtoLord <artgoi111@gmail.com>
2019-07-10 16:52:07 +0300

    Review

--- wiki/tests.py
+++ wiki/tests.py
@@ -16,6 +16,13 @@ class GameOperatorTest(TestCase):
             self.start_time = None
             self.last_action_time = 0
 
+    def generate_operator(self, data):
+        return Operator.GameOperator.deserialize_game_operator(
+            data,
+            self.zim,
+            self.graph,
+        )
+
     def setUp(self) -> None:
         self.zim = Mock()
         self.graph = Mock()
@@ -23,47 +30,33 @@ class GameOperatorTest(TestCase):
         Operator.Game.objects.get = Mock(return_value=self.fake_game)
         Operator.Game.objects.create = Mock(return_value=self.fake_game)
 
-    def test_backward_compatibility_1(self):
-        game_operator = Operator.GameOperator.deserialize_game_operator(
-            [1, 2, True, 3, 4, [5]],
-            self.zim,
-            self.graph
+    def test_backward_compatibility_v1(self):
+        game_operator = self.generate_operator(
+            [1, 2, True, 3, 4, [5]]
         )
         self.assertNotEqual(game_operator, None)
         self.assertEqual(game_operator.game, self.fake_game)
 
-    def test_backward_compatibility_2(self):
-        game_operator = Operator.GameOperator.deserialize_game_operator(
-            [1, 2, True, 3, 4, [5], self.game_id],
-            self.zim,
-            self.graph
+    def test_backward_compatibility_v2(self):
+        game_operator = self.generate_operator(
+            [1, 2, True, 3, 4, [5], self.game_id]
         )
         self.assertNotEqual(game_operator, None)
         self.assertEqual(game_operator.game, self.fake_game)
 
     def test_deserialize_error_with_null_data(self):
-        game_operator = Operator.GameOperator.deserialize_game_operator(
-            None,
-            self.zim,
-            self.graph
-        )
+        game_operator = self.generate_operator(None)
         self.assertEqual(game_operator, None)
 
-    def test_deserialize_error_with_error_data(self):
-        game_operator = Operator.GameOperator.deserialize_game_operator(
-            [1, 2],
-            self.zim,
-            self.graph
-        )
+    def test_deserialize_error_with_wrong_data(self):
+        game_operator = self.generate_operator([1, 2])
         self.assertEqual(game_operator, None)
 
-        game_operator = Operator.GameOperator.deserialize_game_operator(
+        game_operator = self.generate_operator(
             {
                 "test1": "smth",
                 "test2": "smth",
                 "game_id": self.game_id
-            },
-            self.zim,
-            self.graph
+            }
         )
         self.assertEqual(game_operator, None)
