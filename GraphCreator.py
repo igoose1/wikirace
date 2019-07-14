7aba63a5ac394ea18dd36cb4b08c61566f69a339
DanGolov <daniilgolov2016@ya.ru>
2019-07-14 11:09:31 +0300

    updated GraphCreator

--- GraphCreator.py
+++ GraphCreator.py
@@ -3,20 +3,17 @@ import time
 import re
 import urllib.parse as parse
 import sys
+from django.conf import settings
 
 
 def get_links_from_html(data):
-    '''
-    If you need to extract graph from category (not full wikipedia)
-    remove '.html'
-    '''
+    # If you need to extract graph from category (not full wikipedia)
+    # remove '.html'
     pats = re.findall(r'href="[^""]*.html"', data)
     links = []
     for elem in pats:
         elem = elem[6:-1]
-        if elem.startswith('http'):
-            continue
-        else:
+        if not elem.startswith('http'):
             links.append(parse.unquote(elem))
     return links
 
@@ -29,14 +26,14 @@ def int_to_bytes(x):
     return bytes([b1, b2, b3, b4])
 
 
-def generate_graph(x, zim_name, threads_num):
+def generate_graph(x, threads_num):
     if x < 0 or x >= threads_num:
         return
 
-    wikipedia = zimply.zimply.ZIMFile(zim_name, 'utf-8')
-    articleCount = wikipedia.header_fields['articleCount']
+    wikipedia = zimply.zimply.ZIMFile(settings.WIKI_ZIMFILE_PATH, 'utf-8')
+    article_count = wikipedia.header_fields['articleCount']
 
-    used = [-1] * articleCount
+    used = [-1] * article_count
     it_id = 0
 
     edges = open('data/edges' + str(x), 'wb')
@@ -44,12 +41,12 @@ def generate_graph(x, zim_name, threads_num):
     off = 0
 
     start_time = time.time()
-    block = articleCount // threads_num
+    block = article_count // threads_num
     start = x * block
     fin = (x + 1) * block
 
     if x == threads_num - 1:
-        fin = articleCount
+        fin = article_count
     for i in range(start, fin):
         if i % 1000 == 0:
             print(i, "files are ready")
@@ -65,8 +62,8 @@ def generate_graph(x, zim_name, threads_num):
                 entry, index = wikipedia._get_entry_by_url('A', link)
                 it_id += 1
                 while (entry is not None and entry['namespace'] == 'A' and
-                        'redirectIndex' in entry.keys() and
-                        used[index] != it_id):
+                       'redirectIndex' in entry.keys() and
+                       used[index] != it_id):
                     used[index] = it_id
                     index = entry['redirectIndex']
                     entry = wikipedia.read_directory_entry_by_index(index)
@@ -80,14 +77,13 @@ def generate_graph(x, zim_name, threads_num):
     offset.write(int_to_bytes(off))
     offset.close()
     edges.close()
-    print(time.time() - start_time)
+    print('Time of execution: ', time.time() - start_time)
 
 
 '''
 in command line:
 1 argument - id of thread (from 0 to (number_of_threads - 1))
 2 argument - number of threads
-3 argument - name of zim file
 '''
 
-generate_graph(int(sys.argv[1]), sys.argv[3], int(sys.argv[2]))
+generate_graph(int(sys.argv[1]), int(sys.argv[2]))
