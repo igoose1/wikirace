b2f4675bcd246d17cb55de5feeb9f0b33b73271d
DanGolov <daniilgolov2016@ya.ru>
2019-07-14 15:38:47 +0300

    updated get_wiki_page

--- wiki/get_wiki_page.py
+++ wiki/get_wiki_page.py
@@ -178,6 +178,7 @@ def show_path_page(prevars):
     user_path = [start]
     game_id = prevars.game_operator.game.game_id
     turns = Turn.objects.filter(game_id=game_id).order_by('time')
+
     user_path += [prevars.zim_file[turn.to_page_id].title for turn in turns]
     context = {
         'from': our_path[0],
