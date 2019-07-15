31a03d40496b247716aa625637e6e4f7ed84cf3c
Oskar Sharipov <oskar.sharipov@tuta.io>
2019-07-15 17:55:01 +0300

    Add checking that /back won't break tests

--- wiki/tests.py
+++ wiki/tests.py
@@ -248,7 +248,9 @@ class PlayingTest(TestCase):
         ind = 0
         for url in url_way:
             resp = self.client.get(url)
-            if url == back_url:
+
+            # '/back' before playing checks in GetWikiPageTest.testImpossibleBack
+            if url == back_url and ind >= 2:
                 self.assertRedirects(resp, quote(url_way[ind - 2]))
             else:
                 self.assertEqual(resp.status_code, 200)
