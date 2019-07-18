1e069b3bb6119bbbcf1dfae7e47fc59e397f3149
iliamikado <iliamikado@gmail.com>
2019-07-18 14:58:41 +0300

    Good file closing

--- wiki/management/commands/addtrials.py
+++ wiki/management/commands/addtrials.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from contextlib import closing
 from wiki.models import GamePair, Trial
 from django.conf import settings
 import wiki.ZIMFile
@@ -12,17 +13,14 @@ class Command(BaseCommand):
         parser.add_argument('path_to_file', type=str)
 
     def handle(self, *args, **options):
-        zim = wiki.ZIMFile.ZIMFile(
+        with closing(wiki.ZIMFile.ZIMFile(
             settings.WIKI_ZIMFILE_PATH,
-            settings.WIKI_ARTICLES_INDEX_FILE_PATH
-        )
-        file = open(options['path_to_file'], 'r')
-        trials = json.loads(file.read())
-        for trial in trials:
-            game_pair = GamePair.get_or_create(zim[trial['start']].index, zim[trial['end']].index)
-            Trial.objects.get_or_create(
-                trial_name=trial['name'],
-                game_pair=game_pair
-            )
-        zim.close()
-        file.close()
+            settings.WIKI_ARTICLES_INDEX_FILE_PATH)
+        ) as zim, open(options['path_to_file'], 'r') as file:
+            trials = json.loads(file.read())
+            for trial in trials:
+                game_pair = GamePair.get_or_create(zim[trial['start']].index, zim[trial['end']].index)
+                Trial.objects.get_or_create(
+                    trial_name=trial['name'],
+                    game_pair=game_pair
+                )
