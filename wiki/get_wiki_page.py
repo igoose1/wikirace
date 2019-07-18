e483c26e020a67f9b07d559921d3e2253b1da734
LDS2002 <fireman2002lds@gmail.com>
2019-07-18 20:24:46 +0300

    Fix invalid url in share

--- wiki/get_wiki_page.py
+++ wiki/get_wiki_page.py
@@ -234,7 +234,8 @@ def get_end_page(prevars):
         ),
         'name': prevars.settings.name,
         'game_id': prevars.game_operator.game_id,
-        'link': 'dev-wikirace.p.lksh.ru/join_game/' + prevars.game_operator.game.multiplayer.multiplayer_key,
+        'link':
+            prevars.request.META['HTTP_HOST'] + '/join_game/' + prevars.game_operator.game.multiplayer.multiplayer_key,
         'title_text': 'Победа' if not surrendered else 'Игра окончена',
         'results_table': get_results(
             prevars.game_operator.game.multiplayer,
