7c76a52aabf0e5d8bcf4474cbf3561acb690f99a
Сергей <bss.ser007@gmail.com>
2019-12-31 18:13:58 +0300

    info_tool update:
    search by href
    
    follow redirect

--- devtools/management/commands/info_tool.py
+++ devtools/management/commands/info_tool.py
@@ -9,7 +9,27 @@ file = ZIMFile(
 )
 
 
-def run():
+def display_article_info(article):
+    title = article.title
+    url = article.url
+    index = article.index
+    redirecting = article.is_redirecting
+    print('TITLE:       ', title, sep='')
+    print('URL  :       ', url, sep='')
+    print('INDEX  :       ', index, sep='')
+    print('REDIRECTING: ', redirecting, sep='')
+
+
+def show_redirected_info(article):
+    if not article.is_redirecting:
+        return
+
+    print('[REDIRECTING]=============')
+    article = article.follow_redirect()
+    display_article_info(article)
+
+
+def run_id():
     try:
         while True:
             print("Enter article [id]:")
@@ -17,12 +37,40 @@ def run():
             try:
                 idx = int(string)
                 article = file[idx]
-                title = article.title
-                url = article.url
-                redirecting = article.is_redirecting
-                print('TITLE:       ', title, sep='')
-                print('URL  :       ', url, sep='')
-                print('REDIRECTING: ', redirecting, sep='')
+                display_article_info(article)
+                show_redirected_info(article)
+            except:
+                pass
+    except:
+        pass
+
+
+def run_href():
+    try:
+        while True:
+            print("Enter article [href]:")
+            string = input()
+            try:
+                idx = string
+                article = file[idx]
+                display_article_info(article)
+                show_redirected_info(article)
+            except:
+                pass
+    except:
+        pass
+
+
+def run():
+    try:
+        while True:
+            print("Enter ['href'] or ['idx']")
+            string = input()
+            try:
+                if (string == 'href'):
+                    run_href()
+                if (string == 'idx'):
+                    run_id()
             except:
                 pass
     except:
