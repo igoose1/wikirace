4656c5eb8dacf71731dd43d2cb6aa1b4ee9a0bcc
artolord <artgoi111@gmail.com>
2020-01-01 19:10:39 +0300

    add rate to user settings

--- wiki/models.py
+++ wiki/models.py
@@ -22,6 +22,7 @@ class UserSettings(models.Model):
         max_length=256,
         null=False
     )
+    rate = models.FloatField(default=0)
     vk_access_token = models.CharField(
         max_length=256,
         null=False
