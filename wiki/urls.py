10e7d338e1027154b0a099e8e6afcf2b0b2716d3
artolord <artgoi111@gmail.com>
2020-01-04 16:08:18 +0300

    fix game joining

--- wiki/urls.py
+++ wiki/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     url('^$', get_wiki_page.get_main_page),
     url('^feedback', get_wiki_page.get_feedback_page),
-    path('join_game/<str:multiplayer_key>', get_wiki_page.join_game_by_key),
+    # path('join_game/<str:multiplayer_key>', get_wiki_page.join_game_by_key),
     path('results_of_game/<str:multiplayer_key>', get_wiki_page.show_results_table),
     path('results_of_trial/<str:trial_id>', get_wiki_page.show_trial_results_table),
     url('^choose_custom_game', get_wiki_page.choose_custom_game),
