20473db26b3026a4ee543c54f35c22f9a3eb7d1d
artolord <artgoi111@gmail.com>
2020-01-04 11:32:26 +0300

    fix migration 0027

--- wiki/migrations/0027_auto_20200104_1120.py
+++ wiki/migrations/0027_auto_20200104_1120.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('wiki', '0026_auto_20200103_1839'),
+        ('wiki', '0027_auto_20200103_1918'),
     ]
 
     operations = [
