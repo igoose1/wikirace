303c87f550006bd6ab6e8e7674e513507e084466
artolord <artgoi111@gmail.com>
2020-01-05 18:25:13 +0300

    fix pep8

--- restruct_util.py
+++ restruct_util.py
@@ -8,12 +8,13 @@ SIZE = 4
 with open(path1, 'rb') as file1, open(path2, 'wb') as file2:
     data = file1.read(16384)
     while len(data) > 0:
-        file2.write(pack(
-                    "<" + "I" * (len(data) // 4),
-                    *unpack(
-                        ">" + "I" * (len(data) // 4),
-                        data
-                        ))
-                    )
+        file2.write(
+            pack(
+                "<" + "I" * (len(data) // 4),
+                *unpack(
+                    ">" + "I" * (len(data) // 4),
+                    data
+                )
+            )
+        )
         data = file1.read(16384)
-
